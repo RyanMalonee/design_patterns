ae2675e5ffdccc929cd3d3d45147550546021f97
Ryan Malone <ryan.malone192@yahoo.com>
2023-09-12 18:38:35 -0400

    Fully working program

--- decorator/Paint.java
+++ decorator/Paint.java
@@ -5,10 +5,27 @@ import java.util.ArrayList;
 public class Paint extends VehicleDecorator {
   Paint(Vehicle vehicle, String color) {
     super(vehicle.lines);
-    if(color.equalsIgnoreCase("Red")) {
-        vehicle.lines.add(0,"\u001B[31m");
-        
-        //vehicle.lines.add(vehicle.lines.size(), "\u001B[31m");
+    if(color.equalsIgnoreCase("None")) {
+      vehicle.lines.add(0,"\u001B[0m");
+      vehicle.lines.add(vehicle.lines.size(), "\u001B[0m");
+    } else if(color.equalsIgnoreCase("Red")) {
+      vehicle.lines.add(0,"\u001B[31m");
+      vehicle.lines.add(vehicle.lines.size(), "\u001B[0m");
+    } else if (color.equalsIgnoreCase("Green")){
+      vehicle.lines.add(0,"\u001B[32m");
+      vehicle.lines.add(vehicle.lines.size(), "\u001B[0m");
+    } else if (color.equalsIgnoreCase("Yellow")) {
+      vehicle.lines.add(0,"\u001B[33m");
+      vehicle.lines.add(vehicle.lines.size(), "\u001B[0m");
+    } else if (color.equalsIgnoreCase("Blue")) {
+      vehicle.lines.add(0,"\u001B[34m");
+      vehicle.lines.add(vehicle.lines.size(), "\u001B[0m");
+    } else if (color.equalsIgnoreCase("Purple")) {
+      vehicle.lines.add(0,"\u001B[35m");
+      vehicle.lines.add(vehicle.lines.size(), "\u001B[0m");
+    } else if (color.equalsIgnoreCase("Cyan")){
+      vehicle.lines.add(0,"\u001B[36m");
+      vehicle.lines.add(vehicle.lines.size(), "\u001B[0m");
     }
   }
 }
--- decorator/VehicleDecorator.java
+++ decorator/VehicleDecorator.java
@@ -5,16 +5,19 @@ public abstract class VehicleDecorator extends Vehicle {
   public VehicleDecorator(ArrayList<String> lines) {
     super(lines);
   }
-
+  
   protected void integrateDecor(ArrayList<String> decor) {
     System.out.print(lines.get(0));
-    for (int i = 1; i < lines.size(); i++) {
+    for (int i = 1; i < lines.size()-1; i++) {
       for (int j = 0; j < lines.get(i).length(); j++) {
-        if (lines.get(i).charAt(j) != decor.get(i-1).charAt(j) && decor.get(i-1).charAt(j) != ' ' && j < lines.get(i).length()) {
-          lines.set(i, lines.get(i).substring(0, j)+ decor.get(i-1).charAt(j) + lines.get(i).substring(j+1));
+        if (lines.get(i).charAt(j) != decor.get(i - 1).charAt(j) &&
+            decor.get(i - 1).charAt(j) != ' ' && j < lines.get(i).length()) {
+          lines.set(i, lines.get(i).substring(0, j) 
+              + decor.get(i - 1).charAt(j) + lines.get(i).substring(j + 1));
         }
       }
     }
+    System.out.print(lines.get(lines.size()-1));
   }
-    
+  
 }
