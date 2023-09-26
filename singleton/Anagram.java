7e3a4030e5139f52bc2b63a40fde4870a9d02abb
Ryan Malone <ryan.malone192@yahoo.com>
2023-09-26 15:41:06 -0400

    Progress on singleton design pattern

--- singleton/Anagram.java
+++ singleton/Anagram.java
@@ -7,14 +7,20 @@ public class Anagram {
   private ArrayList<String> answers;
 
   public Anagram(String question, ArrayList<String> answers) {
-    
+    this.question = question;
+    this.answers = answers;
   }
 
   public String getQuestion() {
-
+    return this.question;
   }
 
   public boolean isCorrect(String userAnswer) {
-
+    for (String answer : answers) {
+      if (userAnswer.equals(answer)) {
+        return true;
+      }
+    }
+    return false;
   }
 }
--- singleton/Game.java
+++ singleton/Game.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 import java.util.Random;
 
 public class Game {
-  private Game game;
+  private static Game game;
   private Random rand;
   private Difficulty level;
   private HashMap<Difficulty, ArrayList<Anagram>> anagrams;
@@ -13,22 +13,46 @@ public class Game {
   private int score;
   
   private Game() {
-    
+    rand = new Random();
+    level = Difficulty.EASY;
+    anagrams = new HashMap<Difficulty, ArrayList<Anagram>>();
+    // I did use w3 schools for reference here because I've never used a HashMap before
+    anagrams.put(Difficulty.EASY, FileReader.getAnagrams(Difficulty.EASY));
+    anagrams.put(Difficulty.MEDIUM, FileReader.getAnagrams(Difficulty.MEDIUM));
+    anagrams.put(Difficulty.HARD, FileReader.getAnagrams(Difficulty.HARD));
+    score = 0;
   }
   
   public static Game getInstance() {
-    
+    if (game == null) {
+      game = new Game();
+    }
+    return game;
   }
   
   public String getQuestion() {
-    
+    if (score < 5) {
+      level = Difficulty.EASY;
+    } else if (score < 10) {
+      level = Difficulty.MEDIUM;
+    } else {
+      level = Difficulty.HARD;
+    }
+    currentAnagram = anagrams.get(level).get(rand.nextInt(anagrams.get(level).size()));
+    return currentAnagram.getQuestion();
   }
   
   public boolean isCorrect(String userAnswer) {
-    
+    if (currentAnagram.isCorrect(userAnswer)) {
+      score++;
+      return true;
+    } else {
+      score--;
+      return false;
+    }
   }
   
   public int getScore() {
-    
+    return score;
   }
 }
