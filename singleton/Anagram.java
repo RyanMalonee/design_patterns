508ed063d1264f5403bdab06dc0a32377243c076
Ryan Malone <ryan.malone192@yahoo.com>
2023-10-01 14:05:58 -0400

    Added javadoc and output.png

--- singleton/Anagram.java
+++ singleton/Anagram.java
@@ -2,19 +2,41 @@ package singleton;
 
 import java.util.ArrayList;
 
+/**
+ * Creates an anagram object with a question and answers to that question.
+ *
+ * @author Ryan Malone
+ */
 public class Anagram {
   private String question;
   private ArrayList<String> answers;
 
+  /**
+   * Creates an anagram object with a question and answers to that question.
+   *
+   * @param question the question to be asked
+   * @param answers the answers to the question
+   */
   public Anagram(String question, ArrayList<String> answers) {
     this.question = question;
     this.answers = answers;
   }
 
+  /**
+   * Retrieves the question of the anagram.
+   *
+   * @return the question that the current anagram stores
+   */
   public String getQuestion() {
     return this.question;
   }
 
+  /**
+   * Checks to see if the user's answer matches the possible answers of the anagram.
+   *
+   * @param userAnswer the user's answer to the anagrams question
+   * @return true if the user's answer matches the possible answers, false otherwise
+   */
   public boolean isCorrect(String userAnswer) {
     for (String answer : answers) {
       if (userAnswer.equals(answer)) {
--- singleton/Game.java
+++ singleton/Game.java
@@ -4,6 +4,11 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Random;
 
+/**
+ * Creates a single instance of a Game that will be used throughout the program.
+ *
+ * @author Ryan Malone
+ */
 public class Game {
   private static Game game;
   private Random rand;
@@ -12,6 +17,11 @@ public class Game {
   private Anagram currentAnagram;
   private int score;
   
+  /**
+   * Creates a single instance of a Game by creating the anagrams HashMap and
+   * setting all the variables to their default value (e.g. difficulty is easy,
+   * score starts at 0, etc.)
+   */
   private Game() {
     rand = new Random();
     level = Difficulty.EASY;
@@ -23,6 +33,12 @@ public class Game {
     score = 0;
   }
   
+  /**
+   * If there is no instance of a game yet, it will create one. Otherwise, it
+   * will simply return the instance that is already created.
+   *
+   * @return an instance of the Game object
+   */
   public static Game getInstance() {
     if (game == null) {
       game = new Game();
@@ -30,6 +46,13 @@ public class Game {
     return game;
   }
   
+  /**
+   * Retrieves a question of a certain difficulty that is 
+   * based on the current score of the player (as they score more
+   * it gets more difficult questions and vice versa).
+   *
+   * @return the question to be displayed/asked
+   */
   public String getQuestion() {
     if (score < 5) {
       level = Difficulty.EASY;
@@ -41,7 +64,14 @@ public class Game {
     currentAnagram = anagrams.get(level).get(rand.nextInt(anagrams.get(level).size()));
     return currentAnagram.getQuestion();
   }
-  
+
+  /**
+   * Checks if the user's answer is correct and updates the
+   * user's score accordingly.
+   *
+   * @param userAnswer the user's answer to the question
+   * @return true if the user's answer is correct, false otherwise
+   */
   public boolean isCorrect(String userAnswer) {
     if (currentAnagram.isCorrect(userAnswer)) {
       score++;
@@ -52,6 +82,11 @@ public class Game {
     }
   }
   
+  /**
+   * Gets the user's current score.
+   *
+   * @return the score of the user
+   */
   public int getScore() {
     return score;
   }
